1f7d2ce64a1f01b5ff050436bd533398efcab322
Asini <ash9840@nyu.edu>
2025-02-16 23:56:22 +0400

    ss

--- 30MFF/script.ts
+++ 30MFF/script.ts
@@ -1,9 +1,17 @@
+
+
+/*interactive slider to the webpage that adjusts the brightness and contrast of the page based on the slider's value*/
 document.addEventListener("DOMContentLoaded", function () {
-    const slider = document.getElementById("styleSlider") as HTMLInputElement; // Ensure TypeScript recognizes this as an input
+    const slider = document.getElementById("styleSlider") as HTMLInputElement; // ensure TypeScript recognizes this as an input
     const body = document.body;
 
     slider.addEventListener("input", function () {
-        let value = parseInt(slider.value, 10); // Convert value from string to number
+        let value = parseInt(slider.value, 10); // convert value from string to number
         body.style.filter = `brightness(${50 + value * 3}%) contrast(${150 - value * 2}%)`;
     });
+});
+
+/*ensures smooth scrolling through out page*/
+document.addEventListener("DOMContentLoaded", function () {
+    document.documentElement.style.scrollBehavior = "smooth";
 });
